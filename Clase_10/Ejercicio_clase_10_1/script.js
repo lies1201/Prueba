54a3e31dfeaf5471117d1733fb5cdbdf14604562
lies1201 <limairytorrealba@gmail.com>
2024-01-24 13:41:30 +0100

    ejercicio 10 resuelto y cv

--- Clase_10/Ejercicio_clase_10_1/script.js
+++ Clase_10/Ejercicio_clase_10_1/script.js
@@ -3,7 +3,16 @@
 1- Ocultar el enlace Seguir leyendo.
 2- Mostrar el texto incluido dentro del span*/
 
-function muestra() { }
+function muestra() { 
+    
+var mostrar = document.getElementById("adicional");
+mostrar.style.display = "inline";
+var mostrar2 = document.getElementById("enlace2");
+mostrar2.style.display = "inline";
+var ocultar = document.getElementById("enlace1");
+ocultar.style.display = "none";
+   
+}
 
 
 
@@ -11,3 +20,13 @@ function muestra() { }
 
 1- Crear un nuevo evento en el enlace2 que llamará a la función oculta().
 2- Dicha función ocultará el enlace2, el texto el span y volverá a mostrar el enlace Seguir leyendo.*/
+function oculta(){
+
+    var ocultar = document.getElementById("adicional");
+    ocultar.style.display = "none";
+    var ocultar2 = document.getElementById("enlace2");
+    ocultar2.style.display = "none";
+    var mostrar = document.getElementById("enlace1");
+    mostrar.style.display = "inline";
+
+  }
