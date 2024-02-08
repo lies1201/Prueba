f6eb5f7795de0e557f6d50b93b56f594408f2142
lies1201 <limairytorrealba@gmail.com>
2024-02-08 13:03:56 +0100

    clase jueves

---
+++ Clase_17_1/script.js
@@ -0,0 +1,4 @@
+let misConocimientos = ['variables', 'operadores', 'estructuras de control', 'funciones'];
+let aprendido = ['spread', 'local storage'];
+let misConocimientosAmpliados = [...misConocimientos, ...aprendido, 'otra cosa más'];
+console.log(misConocimientosAmpliados);
